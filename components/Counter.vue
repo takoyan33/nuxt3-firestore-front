cc2a8176d642dfbb9bc3eb812a30df5c79a33ab4
takoyan33 <harrier2070@gmail.com>
2023-03-30 10:59:53 +0900

    storybookの導入

---
+++ components/Counter.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+// クリックしたらカウントUPするだけのボタン
+const state = ref(0);
+const increment = () => {
+  state.value++;
+};
+</script>
+
+<template>
+  <div>Count: {{ state }}</div>
+  <button @click="increment">Increment</button>
+</template>
---
+++ components/stories/Header.stories.ts
@@ -0,0 +1,14 @@
+import { Story } from "@storybook/vue3";
+import Header from "../Header.vue";
+
+export default {
+  title: "components/Header",
+  component: Header,
+};
+
+const Template: Story<typeof Header> = () => ({
+  components: { Header },
+  template: "<Header />",
+});
+
+export const Default = Template.bind({});
