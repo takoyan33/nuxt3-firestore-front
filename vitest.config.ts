05e29f513bbd3f760b40c1c186685df4193ec001
takoyan33 <harrier2070@gmail.com>
2023-03-30 09:40:21 +0900

    テスト完成

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,15 @@
-/// <reference types="vitest" />
-
 import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
-
+import path from "path";
 export default defineConfig({
   plugins: [Vue()],
   test: {
     globals: true,
     environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
