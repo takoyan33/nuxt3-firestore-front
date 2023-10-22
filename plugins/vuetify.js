5fdca38ad1a05e0897ecde25196d08ff422dd19f
abeshumpei <s_abe@gacci.jp>
2023-10-23 01:20:57 +0900

    デプロイ修正

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,20 +1,10 @@
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import Vue from "vue";
-import Vuetify from "vuetify/lib/framework";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
-    components
-  })
+    components,
+  });
 
-  nuxtApp.vueApp.use(vuetify)
-})
-
-
-Vue.use(Vuetify);
-
-// オプションをエクスポートします。といっても、今回は特にオプション設定していないので空オブジェクトです。
-export const options = {}
-
-export default new Vuetify(options);
+  nuxtApp.vueApp.use(vuetify);
+});
