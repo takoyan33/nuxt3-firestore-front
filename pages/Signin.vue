a8fc794aa00ba3aa24e2edfabc834c90d74c0ce9
abeshumpei <s_abe@gacci.jp>
2024-01-10 23:12:09 +0900

    ボタンのスタイルの変更

--- pages/Signin.vue
+++ pages/Signin.vue
@@ -1,18 +1,35 @@
 <template>
   <v-container>
-    <h2 class="text-h4 my-4 text-center">SignIn</h2>
+    <h2 class="text-h4 my-4 text-center">ログイン</h2>
     <v-card class="my-5 p-4">
-      <v-card-text class="py-2">
-        このアプリはNuxt.jsを用いたTodoアプリです。主にTodoの検索や確認ができ、他のページでは、APIの取得ができます。
-        <ul>
-          <li class="py-2">
-            使用技術：フロントエンド：Vue.js,Nuxt3、バックエンド：Firestore
-          </li>
-          <li class="py-2">CSS：Vuetify</li>
-          <li class="py-2">テスト：vitest</li>
-          <li class="py-2">コンポーネント管理：Storybook</li>
-        </ul>
-      </v-card-text>
+      <v-text-field v-model="email" label="Email"></v-text-field>
+      <v-text-field v-model="password" label="Password" type="password"></v-text-field>
+      <v-btn @click="login" color="primary">ログイン</v-btn>
     </v-card>
   </v-container>
 </template>
+
+<script>
+import { auth } from '@/firebase.js'
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async login() {
+      try {
+        await auth.signInWithEmailAndPassword(this.email, this.password)
+        // On successful login, you might want to redirect the user to another page
+        // this.$router.push('/dashboard') // Example redirection
+      } catch (error) {
+        console.error('Login error:', error)
+        // Handle login error (show a message to the user, etc.)
+      }
+    }
+  }
+}
+</script>
